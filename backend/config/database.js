fed92bacc79a4d6055b10f63a47dc432c35003eb
Brandon Forster <brandonforster@live.com>
2021-02-22 09:32:56 -0700

    Success on deploy

--- backend/config/database.js
+++ backend/config/database.js
@@ -40,6 +40,11 @@ module.exports = {
     host,
     dialect: 'postgres',
     seederStorage: 'sequelize',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
     dialectOptions: {
       ssl: {
         require: true,
