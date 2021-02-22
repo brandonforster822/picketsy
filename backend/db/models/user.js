9ccc5ea199ac7e6dee8ec997acb290d845965c8f
Brandon Forster <brandonforster@live.com>
2021-02-22 11:18:28 -0700

    Fixed config variable in db

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -46,8 +46,8 @@ module.exports = (sequelize, DataTypes) => {
       },
     },
   });
-  User.prototype.toSafeObject = function() { // remember, this cannot be an arrow function
-    const { id, username, email } = this; // context will be the User instance
+  User.prototype.toSafeObject = function() {
+    const { id, username, email } = this;
     return { id, username, email };
   };
   User.prototype.validatePassword = function (password) {
